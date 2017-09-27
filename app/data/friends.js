18948468e4fd52bd2697fb5fba63cab3fc528a58
Jakie Rice <jakie.rice@gmail.com>
2017-09-27 18:29:07 -0500

    Change friends and friends' survey answers

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,82 +1,82 @@
 var friends = [
 	{
-		name: "Daniel Hernandez",
-		photo: "https://avatars3.githubusercontent.com/u/28367077?v=4&s=400",
+		name: "Hank Hill",
+		photo: "https://images-na.ssl-images-amazon.com/images/M/MV5BMzE4MjQyMDUyOF5BMl5BanBnXkFtZTYwNTU3OTQ3._V1_SY256_SX225_AL_.jpg",
 		scores: [
+			5,
+			2,
+			4,
 			1,
 			1,
 			1,
 			1,
+			2,
 			1,
-			1,
-			1,
-			1,
-			1,
-			1
-		]
-	},
-	{
-		name: "Latisha McNeel",
-		photo: "https://avatars3.githubusercontent.com/u/2047327?v=4&s=400",
-		scores: [
-			2,
-			2,
-			2,
-			2,
-			2,
-			2,
-			2,
-			2,
-			2,
-			2
-		]
-	},
-	{
-		name: "Kathryn Herod",
-		photo: "https://avatars3.githubusercontent.com/u/7751467?v=4&s=400",
-		scores: [
-			3,
-			3,
-			3,
-			3,
-			3,
-			3,
-			3,
-			3,
-			3,
 			3
 		]
 	},
 	{
-		name: "Jacob Thomas",
-		photo: "https://avatars3.githubusercontent.com/u/14240957?v=4&s=400",
+		name: "Archer Sterling",
+		photo: "http://cdn.akamai.steamstatic.com/steamcommunity/public/images/avatars/aa/aa6fc55881da94ec41c505debeb4f316cc1cadb1_full.jpg",
 		scores: [
+			5,
+			5,
+			5,
+			5,
+			3,
+			5,
 			4,
+			1,
 			4,
-			4,
-			4,
-			4,
-			4,
-			4,
-			4,
-			4,
-			4
+			5
 		]
 	},
 	{
-		name: "Camden Kirkland",
-		photo: "https://avatars1.githubusercontent.com/u/6783131?v=4&s=400",
+		name: "Erlich Bachman",
+		photo: "http://www.piedpiper.com/app/themes/pied-piper/dist/images/erlich.png",
 		scores: [
+			3,
+			4,
+			3,
+			4,
 			5,
 			5,
+			1,
+			2,
+			3,
+			1
+		]
+	},
+	{
+		name: "Red Reddington",
+		photo: "http://cimg.tvgcdn.net/i/r/2013/09/23/d995a35c-58d4-4b32-bfc7-c7b1d55948ac/resize/350x241/676d18b82685b1ea3a458c6818f4f9ba/130922james-spader2.jpg",
+		scores: [
+			2,
 			5,
+			4,
+			2,
+			1,
+			3,
 			5,
-			5,
-			5,
-			5,
-			5,
+			3,
 			5,
 			5
+		]
+	},
+	{
+		name: "Michael Scott",
+		photo: "https://upload.wikimedia.org/wikipedia/en/d/dc/MichaelScott.png",
+		scores: [
+			2,
+			4,
+			1,
+			2,
+			1,
+			1,
+			5,
+			4,
+			1,
+			1
 		]
 	}
 ];
